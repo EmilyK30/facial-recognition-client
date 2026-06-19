9356d1cc5331fa915cbfb1d93bc885674848f549
Emilie Karol Seck <emiliekces@gmail.com>
2026-06-19 11:20:53 +0000

    Ajout du routing et des pages squelettes (Home, Register, Recognize, Search)

---
+++ src/pages/Recognize.jsx
@@ -0,0 +1,15 @@
+/**
+ * Page Reconnaissance - Maguette
+ * A completer : WebcamCapture + appel recognizeFace() depuis services/api.js
+ * + affichage des resultats (fiche + score, ou "aucune correspondance")
+ */
+function Recognize() {
+  return (
+    <div>
+      <h1>Reconnaissance d'un visage</h1>
+      <p>Page en construction (Maguette).</p>
+    </div>
+  );
+}
+
+export default Recognize;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,15 @@
+/**
+ * Page Enrolement - Lissa
+ * A completer : formulaire (numero_dossier, prenom, nom, adresse, date_naissance)
+ * + WebcamCapture + appel registerPerson() depuis services/api.js
+ */
+function Register() {
+  return (
+    <div>
+      <h1>Enrolement d'une personne</h1>
+      <p>Page en construction (Lissa).</p>
+    </div>
+  );
+}
+
+export default Register;
